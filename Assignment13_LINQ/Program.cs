9e15b9c1d38ad1a92b9e97ecd4544dd268e28391
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-10 11:53:15 +0300

    p2

--- Assignment13_LINQ/Program.cs
+++ Assignment13_LINQ/Program.cs
@@ -11,11 +11,27 @@ namespace Assignment13_LINQ
 
             #region p1: Get first Product out of Stock 
 
+            //var product = ListGenerators.ProductList;
+
+            //var FristOutOfStock = product.First(p => p.UnitsInStock == 0);
+
+            //Console.WriteLine($"First Out of Stock Product: {FristOutOfStock.ProductName }");
+
+            #endregion
+
+            #region p2: . Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
             var product = ListGenerators.ProductList;
+            var firstExpensiveProduct = product.FirstOrDefault(p => p.UnitPrice > 1000);
+            if (firstExpensiveProduct != null)
+            {
+                Console.WriteLine($"Found: {firstExpensiveProduct.ProductName} - ${firstExpensiveProduct.UnitPrice}");
+            }
+            else
+            {
+                Console.WriteLine("No product found over $1000.");
+            }
 
-            var FristOutOfStock = product.First(p => p.UnitsInStock == 0);
-
-            Console.WriteLine($"First Out of Stock Product: {FristOutOfStock.ProductName }");
 
             #endregion
         }
