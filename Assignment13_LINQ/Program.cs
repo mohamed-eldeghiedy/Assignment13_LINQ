4f503351412485b5612290265f5ede0b29770541
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-10 11:46:54 +0300

    p1

--- Assignment13_LINQ/Program.cs
+++ Assignment13_LINQ/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Assignment13_LINQ
+﻿
+using System.Collections.Generic;
+namespace Assignment13_LINQ
 {
     internal class Program
     {
         static void Main()
         {
-           var prodout = 
+
+            //q1 : Element Operators
+
+            #region p1: Get first Product out of Stock 
+
+            var product = ListGenerators.ProductList;
+
+            var FristOutOfStock = product.First(p => p.UnitsInStock == 0);
+
+            Console.WriteLine($"First Out of Stock Product: {FristOutOfStock.ProductName }");
+
+            #endregion
         }
     }
 }
