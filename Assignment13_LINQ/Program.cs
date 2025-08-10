f23b705b40d3f9889220a476e27c73fe19f8b37c
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-10 13:33:01 +0300

    p14

--- Assignment13_LINQ/Program.cs
+++ Assignment13_LINQ/Program.cs
@@ -169,18 +169,40 @@ namespace Assignment13_LINQ
 
             #region p10:Get the cheapest price among each category's products
 
-            var result = ListGenerators.ProductList;
-            var cheapestPrices = result
-                .GroupBy(p => p.Category)
-                .Select(g => new
-                {
-                    Category = g.Key,
-                    CheapestPrice = g.Min(p => p.UnitPrice)
-                })
-                .ToList();
-            foreach (var item in cheapestPrices)
+            //var result = ListGenerators.ProductList;
+            //var cheapestPrices = result
+            //    .GroupBy(p => p.Category)
+            //    .Select(g => new
+            //    {
+            //        Category = g.Key,
+            //        CheapestPrice = g.Min(p => p.UnitPrice)
+            //    })
+            //    .ToList();
+            //foreach (var item in cheapestPrices)
+            //{
+            //    Console.WriteLine($"Cheapest price in {item.Category} category: ${item.CheapestPrice}");
+            //}
+
+
+            #endregion
+
+            #region p11: Get the products with the cheapest price in each category (Use Let)
+
+            var cheapestProducts =
+               from product in ListGenerators.ProductList
+               group product by product.Category into categoryGroup
+               let minPrice = categoryGroup.Min(p => p.UnitPrice)
+               from p in categoryGroup
+               where p.UnitPrice == minPrice
+               select new
+               {
+                   Category = categoryGroup.Key,
+                   ProductName = p.ProductName,
+                   UnitPrice = p.UnitPrice
+               };
+            foreach (var item in cheapestProducts)
             {
-                Console.WriteLine($"Cheapest price in {item.Category} category: ${item.CheapestPrice}");
+                Console.WriteLine($"Category: {item.Category}, Product: {item.ProductName}, Price: ${item.UnitPrice}");
             }
 
 
